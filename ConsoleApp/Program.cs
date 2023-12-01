291df989d6dc3b718bd222839bd14574b52b1fb2
Benke <henrik_junior@hotmail.com>
2023-12-01 16:11:35 +0100

    Tried to add a break in chain from main github branch.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -7,7 +7,11 @@
             Console.WriteLine("Yo tell me you age now!");
             int Age = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Thats right muddafukka, {0} are too young!", Age);
+
+            Console.WriteLine(Age + Age);
+
             Console.ReadKey();
+
         }
     }
 }
