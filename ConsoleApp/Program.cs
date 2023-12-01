c1fa279ddf3fb7bf775d0a121b5c53c4c39117f4
Benke <henrik_junior@hotmail.com>
2023-12-01 16:04:12 +0100

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Yo tell me you age now!");
+            int Age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Thats right muddafukka, {0} are too young!", Age);
+            Console.ReadKey();
+        }
+    }
+}
