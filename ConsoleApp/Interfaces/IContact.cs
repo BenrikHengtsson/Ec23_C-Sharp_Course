8e284b51c3d82018651d06c7bcc5d4ca8e33c198
Benke <henrik_junior@hotmail.com>
2023-12-30 17:27:08 +0100

    Set up some structure folder for console app.

---
+++ ConsoleApp/Interfaces/IContact.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp.Interfaces
+{
+    public interface IContact
+    {
+        string Address { get; set; }
+        string City { get; set; }
+        string Email { get; set; }
+        string FirstName { get; set; }
+        int Id { get; set; }
+        string LastName { get; set; }
+        string? PhoneNumber { get; set; }
+        string PostalCode { get; set; }
+    }
+}
---
+++ ConsoleApp/Models/Contact.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleApp.Interfaces;
+
+namespace ConsoleApp.Models
+{
+    public class Contact : IContact
+    {
+        public int Id { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public string City { get; set; } = null!;
+        public string PostalCode { get; set; } = null!;
+
+
+
+
+    }
+}
